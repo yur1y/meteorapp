6666c39b087fd14fffd0d8b5b01c27b51e7d241a
yur1y <yuriyiskiv@gmail.com>
2016-11-02 21:43:59 +0200

    emails

--- api/users.js
+++ api/users.js
@@ -2,14 +2,14 @@ Accounts.onCreateUser(function (options, user) {
     if (options.profile) {
         user.profile = options.profile;
     }
-    // if( user.services.google){
-    //     Meteor.call('users.onRegister', user.services.google.email
-    //         //          path to the users email in their profile
-    //     );
-    // }
-    // if (user.services.vk) {
-    //     Meteor.call('users.onRegister', user.services.vk.email);
-    // }
+    if( user.services.google){
+        Meteor.call('users.onRegister', user.services.google.email
+            //          path to the users email in their profile
+        );
+    }
+    if (user.services.vk) {
+        Meteor.call('users.onRegister', user.services.vk.email);
+    }
     user.groups = [];
     user.items = [];
 
