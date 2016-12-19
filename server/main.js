1b0a296e38cb01e252b4e7447d3710c84a424f7f
yur1y <yuriyiskiv@gmail.com>
2016-12-19 23:27:32 +0200

    commit

--- server/main.js
+++ server/main.js
@@ -6,22 +6,9 @@ import {configure} from './configure'
 
 Meteor.startup(function () {
     return Meteor.isServer ?
-
         [
             configure(),
             secure(),
-
-
-        ] : sAlert.config({
-        position: 'bottom-right',
-        timeout: 5000,
-        html: false,
-        onRouteClose: false,
-        stack: true,
-
-        offset: 10, // in px - will be added to first alert (bottom or top - depends of the position in config)
-        beep: false,
-
-    })
-
+        ]
+        : null
 });
